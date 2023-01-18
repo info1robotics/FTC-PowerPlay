b363226644fcb1e8555bb9fe2ba7391b60772343
plex <plex@pop-os.localdomain>
2023-01-18 19:12:37 +0200

    meepmeep

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -0,0 +1,28 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MyClass {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+            // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+            .setConstraints(50, 50, Math.toRadians(350), Math.toRadians(120), 11)
+            .followTrajectorySequence(drive ->
+        drive.trajectorySequenceBuilder(new Pose2d(-35, -62, Math.toRadians(90)))
+                .lineToConstantHeading(new Vector2d(-35, 0))
+                        .build()
+        );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f)
+            .addEntity(myBot)
+            .start();
+    }
+}
