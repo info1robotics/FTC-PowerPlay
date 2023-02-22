74222ed7b7cfe90e13ffa81cc0e56d77f110edd9
Alex <18037145+salcodes1@users.noreply.github.com>
2023-02-22 13:35:56 +0200

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DrivetrainSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DrivetrainSubsystem.java
@@ -2,32 +2,59 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.teamcode.drive.PPController;
 import org.firstinspires.ftc.teamcode.moonshine.Subsystem;
 import org.firstinspires.ftc.teamcode.moonshine.annotations.RequireHardware;
 
 public class DrivetrainSubsystem extends Subsystem {
 
-    double x, y, turn;
+    double x, y, turn, speed;
+    PPController controller;
 
     @RequireHardware(hardwareName = "motorFL") DcMotorEx motorFL;
     @RequireHardware(hardwareName = "motorFR") DcMotorEx motorFR;
     @RequireHardware(hardwareName = "motorBL") DcMotorEx motorBL;
     @RequireHardware(hardwareName = "motorBR") DcMotorEx motorBR;
 
-    public void setMoveVector(double x, double y, double turn) {
+    public void setMoveVector(double x, double y, double turn, double speed) {
         this.x = x;
         this.y = y;
         this.turn = turn;
+        this.speed = speed;
     }
 
     @Override
     protected void onStart() {
-
+        setMoveVector(0, 0, 0, 0);
+        controller = new PPController();
     }
 
     @Override
     protected void onTick() {
-        motorFL.setPower();
+        double[] powers = getMotorPowersArray();
+        motorFL.setPower(powers[0]);
+        motorBL.setPower(powers[1]);
+        motorFR.setPower(powers[2]);
+        motorBR.setPower(powers[3]);
+    }
+
+    double[] getMotorPowersArray() {
+        double[] powers = new double[] {
+            y + x + turn,
+            y - x + turn,
+            y - x - turn,
+            y + x - turn
+        };
+
+        double max = 0;
+        for(double d : powers) {
+            max = Math.max(d, max);
+        }
+        for(int i = 0; i < powers.length; i++) {
+            powers[i] *= 1 / max * speed;
+        }
+
+        return powers;
     }
 
     @Override
